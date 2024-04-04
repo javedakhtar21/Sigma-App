45cbaabc8f71d7ffc1df189afe4723c16bb8cdf0
Md Javed Akhtar <mdjavedakhtar7323@gmail.com>
2024-04-05 00:33:09 +0530

    modified app.jsx

--- Sigma-App/src/App.jsx
+++ Sigma-App/src/App.jsx
@@ -1,15 +1,6 @@
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import "./App.css";
 import UserRegister from "./Components/UserRegister";
-<<<<<<< HEAD
-import NavBar from "./Components/NavBar"
-function App() {
-  return (
-    <>
-      <NavBar />
-      {/* <UserRegister /> */}
-    </>
-=======
 import Login from "./Components/userLogin"
 
 
@@ -21,7 +12,6 @@ function App() {
       <Route path='/login' element={<Login/>}/>
     </Routes>
     </BrowserRouter>
->>>>>>> 43e687fd859ce53982515fd4abf856b047e39b0d
   );
 }
 

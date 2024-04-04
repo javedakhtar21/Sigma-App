7dc14b01a98ef25521ad6804b875f33edabb0d26
BhavyaMadaan11 <bhavyamadaan11@gmail.com>
2024-04-04 23:56:20 +0530

    Login page created and paths set.

--- Sigma-App/src/App.jsx
+++ Sigma-App/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import "./App.css";
 import UserRegister from "./Components/UserRegister";
+import Login from "./Components/userLogin"
+
+
 function App() {
   return (
-    <>
-      <UserRegister />
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/signup' element={<UserRegister/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 

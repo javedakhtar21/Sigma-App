82f993ee42abdcc5a68bfcfdfeafa656e857a880
Md Javed Akhtar <mdjavedakhtar7323@gmail.com>
2024-04-05 16:25:37 +0530

    navbar1 code maintainability increased

--- Sigma-App/src/Components/NavBar.jsx
+++ Sigma-App/src/Components/NavBar.jsx
@@ -11,10 +11,12 @@ const NavBar = () => {
   const iconCss =
     "text-3xl hover:border-2 hover:border-violet-700 hover:rounded-full h-14 w-14 hover:text-violet-700";
   const outerDivCss = "flex flex-row gap-4 justify-around";
+  const innerDivCss='flex flex-initial w-36 p-2 justify-between items-center';
+  const logoCss='font-bold text-3xl uppercase hover:text-violet-700 hover:underline';
   return (
     <div>
       <div className={outerDivCss}>
-        <div className="flex flex-initial w-36 p-2 justify-between items-center">
+        <div className={innerDivCss}>
           <button className={iconCss}>
             <FontAwesomeIcon icon={faMagnifyingGlass} />
           </button>
@@ -23,13 +25,13 @@ const NavBar = () => {
           </button>
         </div>
 
-        <div className="flex flex-initial w-72 p-2 justify-center items-center">
-          <button className="font-bold text-3xl uppercase hover:text-violet-700 hover:underline">
+        <div className={innerDivCss}>
+          <button className={logoCss}>
             Sigma
           </button>
         </div>
 
-        <div className="flex flex-initial w-36 p-2 justify-between items-center">
+        <div className={innerDivCss}>
           <button className={iconCss}>
             <FontAwesomeIcon icon={faUser} />
           </button>

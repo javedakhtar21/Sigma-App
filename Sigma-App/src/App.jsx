d195a8157bcb367ca0febb045a981269d20d2205
BhavyaMadaan11 <bhavyamadaan11@gmail.com>
2024-04-06 15:07:06 +0530

    Subscription added successfully

--- Sigma-App/src/App.jsx
+++ Sigma-App/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import "./App.css";
 import UserRegister from "./Components/UserRegister";
 import Login from "./Components/userLogin"
+import Subscribe from "./Components/subscribe";
 
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
       <Route path='/signup' element={<UserRegister/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
+    <Subscribe/>
     </BrowserRouter>
+    
   );
 }
 
